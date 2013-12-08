4e1f97a30a167ebea10b2a4225c9159693a1bb3d
Graham Campbell <graham@mineuk.com>
2013-12-08 16:27:42 +0000

    Syntax derp

--- src/views/logviewer.blade.php
+++ src/views/logviewer.blade.php
@@ -140,7 +140,7 @@ LogViewer
                 <p>Are you sure you wish to continue?</p>
             </div>
             <div class="modal-footer">
-                {{ HTML::link($url . '/' . $path . '/' . $sapi_plain . '/' . $date . '/delete', 'Yes'), array('class' => 'btn btn-success')) }}
+                {{ HTML::link($url.'/'.$path.'/'.$sapi_plain.'/'.$date.'/delete', 'Yes', array('class' => 'btn btn-success')) }}
                 <button class="btn btn-danger" data-dismiss="modal">No</button>
             </div>
         </div>
