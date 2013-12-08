0c4fb322fb585863b4cfd8e525984430aaef58a5
Graham Campbell <graham@mineuk.com>
2013-12-08 18:47:51 +0000

    Added javascript data loading code

--- src/assets/js/cms-logviewer.js
+++ src/assets/js/cms-logviewer.js
@@ -1,3 +1,23 @@
 $(document).ready(function() {
     $('div[id^="collapse-"]').collapse('show');
+    $.ajax({
+        url: cmsLogViewerURL,
+        type: "GET",
+        dataType: "html",
+        timeout: 10000,
+        success: function(data, status, xhr) {
+            var area = $('#data');
+            area.fadeOut(200, function() {
+                area.html(data);
+                area.fadeIn(200);
+            });
+        },
+        error: function(xhr, status, error) {
+            var area = $('#data');
+            area.fadeOut(200, function() {
+                area.html('<p class="lead">There was an error getting the data</p>');
+                area.fadeIn(200);
+            });
+        }
+    });
 });
